1c1a665bc5f4306afa54d1b73b3372547084b936
Mackenzie Chisholm <chisholm.mackenzie@gmail.com>
2021-07-01 22:23:12 -0400

    project code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./index.css";
+import Header from "./components/Header";
+import Container from "./components/Container/index";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Container />
+    </>
+  );
+};
+
+export default App;
